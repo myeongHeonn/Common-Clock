f1d0cd239d3b05284555bddd63f3f02e81867d9d
myeongheon <axm0219@naver.com>
2024-06-05 12:49:40 +0900

    initial setting

---
+++ CommonClock/src/main/java/fourmation/CommonClock/CommonClockApplication.java
@@ -0,0 +1,13 @@
+package fourmation.CommonClock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonClockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonClockApplication.class, args);
+	}
+
+}
