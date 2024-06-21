88b77ba1d66561213268144b9315fab754f94a13
myeongheon <axm0219@naver.com>
2024-06-22 02:32:45 +0900

    refactor: 팀 컨트롤러 공통응답

--- CommonClock/src/main/java/fourmation/CommonClock/controller/TeamController.java
+++ CommonClock/src/main/java/fourmation/CommonClock/controller/TeamController.java
@@ -1,14 +1,17 @@
 package fourmation.CommonClock.controller;
 
+import fourmation.CommonClock.common.ApiResponse;
+import fourmation.CommonClock.common.ApiResponseBody.SuccessBody;
+import fourmation.CommonClock.common.ApiResponseGenerator;
+import fourmation.CommonClock.common.SuccessMessage;
 import fourmation.CommonClock.dto.request.AddTeamRequest;
 import fourmation.CommonClock.dto.request.TeamLoginRequest;
+import fourmation.CommonClock.dto.response.AddTeamResponse;
 import fourmation.CommonClock.dto.response.TeamLoginResponse;
 import fourmation.CommonClock.service.TeamService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,18 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TeamController {
 
-    @Autowired
-    TeamService teamService;
+    private final TeamService teamService;
 
     @PostMapping("/signup")
-    public ResponseEntity<Long> signup(@RequestBody AddTeamRequest request) {
+    public ApiResponse<SuccessBody<AddTeamResponse>> signup(@RequestBody AddTeamRequest request) {
         Long teamPk = teamService.save(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(teamPk);
+        AddTeamResponse addTeamResponse = new AddTeamResponse(teamPk);
+        return ApiResponseGenerator.success(addTeamResponse, HttpStatus.OK, SuccessMessage.CREATE);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<TeamLoginResponse> login(@RequestBody TeamLoginRequest request) {
+    public ApiResponse<SuccessBody<TeamLoginResponse>> login(@RequestBody TeamLoginRequest request) {
         TeamLoginResponse teamLoginResponse = teamService.login(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(teamLoginResponse);
+        return ApiResponseGenerator.success(teamLoginResponse, HttpStatus.OK, SuccessMessage.LOGIN);
     }
 }
---
+++ CommonClock/src/main/java/fourmation/CommonClock/dto/response/AddTeamResponse.java
@@ -0,0 +1,10 @@
+package fourmation.CommonClock.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AddTeamResponse {
+    private Long teamPk;
+}
